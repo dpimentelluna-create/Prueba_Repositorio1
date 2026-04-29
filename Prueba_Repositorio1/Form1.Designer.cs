5fe99fa2f4c60a786b93b900d3c81ee5e5619019
07CHMK1PA111A07 <07CHMK1PA111A07@07CHMK1PA111A07>
2026-04-29 08:48:06 -0500

    Commit.1

--- Prueba_Repositorio1/Form1.Designer.cs
+++ Prueba_Repositorio1/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(218, 155);
+            this.comboBox1.Location = new System.Drawing.Point(244, 179);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
